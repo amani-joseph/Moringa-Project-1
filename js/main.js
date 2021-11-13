c38152a184a3781b97e1a25614dd3a49175134d6
Joseph Amani <77071817+amani-joseph@users.noreply.github.com>
2021-11-13 22:52:42 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,35 @@
+// # SMOOTH SCROLL
+window.addEventListener('scroll',() => {
+    const scrolled = window.scrollY;
+    console.log("scrolled");
+} );
+
+// # SMOOTH SCROLL END
+
+//!BORDER FOR PRODUCT CARDS
+
+
+
+//!BORDER FOR PRODUCT CARDS END
+
+
+
+
+
+
+
+
+//* SCROLL TO TOP START
+const scrollBtn = document.querySelector(".scroll-to-top");
+
+
+scrollBtn.addEventListener('click', () => {
+    document.documentElement.scrollTop = 0;
+});
+
+//* SCROLL TO TOP END
+
+
+
+
+// const scrollToNext = document.querySelector(".scroll");
