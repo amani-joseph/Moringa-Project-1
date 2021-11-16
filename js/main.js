ef0fbd86f4303507812137d0ec0d5fa90e138508
Joseph Amani <77071817+amani-joseph@users.noreply.github.com>
2021-11-16 05:03:32 +0300

    Add files via upload
    Final responsive and interactive website

---
+++ js/main.js
@@ -0,0 +1,92 @@
+  /* LANDING ANIMATION */
+// Wrap every letter in a span
+var textWrapper = document.querySelector('.ml6 .letters');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+anime.timeline({loop: true})
+  .add({
+    targets: '.ml6 .letter',
+    translateY: ["1.1em", 0],
+    translateZ: 0,
+    duration: 750,
+    delay: (el, i) => 50 * i
+  }).add({
+    targets: '.ml6',
+    opacity: 0,
+    duration: 1000,
+    easing: "easeOutExpo",
+    delay: 1000
+  });
+
+
+
+
+//!  NAVBAR ON SCROLL 
+$(document).ready(function() {
+    // Transition effect for navbar 
+    $(window).scroll(function() {
+      // checks if window is scrolled more than 500px, adds/removes solid class
+      if($(this).scrollTop() > 500) { 
+          $('.navbar').addClass('solid');
+      } else {
+          $('.navbar').removeClass('solid');
+      }
+    });
+});
+
+//!NAVBAR 
+
+
+
+var dark = document.querySelector('nav');
+dark.addEventListener('scroll', () => {
+    dark.style.backgroundColor = "rgba(219, 215, 171, 0.993)";
+});
+
+//! DARKEN NAVBAR ON SCROLL 
+// *PRODUCT FILTERS
+let men = document.querySelector('#inlineCheckbox1');
+let women = document.querySelector('#inlineCheckbox2');
+
+let allProducts = document.querySelectorAll('#product');
+let mensProducts = document.querySelectorAll('#productMen');
+let womenProducts = document.querySelectorAll('#productWomen');
+
+
+
+// *PRODUCT FILTERS
+
+
+// # SMOOTH SCROLL
+window.addEventListener('scroll',() => {
+    const scrolled = window.scrollY;
+    console.log("scrolled");
+} );
+
+// # SMOOTH SCROLL END
+
+//!BORDER FOR PRODUCT CARDS
+
+
+
+//!BORDER FOR PRODUCT CARDS END
+
+
+
+
+
+
+
+
+//* SCROLL TO TOP START
+const scrollBtn = document.querySelector(".scroll-to-top");
+
+
+scrollBtn.addEventListener('click', () => {
+    document.documentElement.scrollTop = 0;
+});
+
+//* SCROLL TO TOP END
+
+
+
